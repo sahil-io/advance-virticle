e67bb110897aaec08aed5747da63b6db894fa74e
sahil-io <sahil@nginr.io>
2023-02-13 20:18:25 -0700

    add manifest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,27 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ThemeProvider, DefaultTheme } from 'styled-components'
+import type {AppProps} from 'next/app'
+import {ThemeProvider, DefaultTheme} from 'styled-components'
+import Head from "next/head";
 
 const theme: DefaultTheme = {
-  colors: {
-    primary: '#111',
-    secondary: '#0070f3',
-  },
+    colors: {
+        primary: '#111',
+        secondary: '#0070f3',
+    },
 }
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-      <>
-        <ThemeProvider theme={theme}>
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </>
-  )
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <Head>
+                    <link rel="manifest" href="/manifest.webmanifest"/>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                </Head>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    )
 }
